a96415ba3356502620c1ff1de6a1d41ccda754dc
fredp613 <fredp613@gmail.com>
2013-09-26 14:58:03 -0400

    fixed pagination in search-fixed js formodal

--- app/assets/javascripts/clients.js
+++ app/assets/javascripts/clients.js
@@ -1,22 +1,4 @@
 
-var createModal = function(link, title) {
-  $('.modal-title').html(title);
-  $('<div id="modalId" />').appendTo('.modal-body');
-  $('#modalId').load(link);
-  $('#myModal').modal();	
 
-} 
-
-/**/ 
-$('.newProject').on('click', function() {
-      var link = $(this).attr('href') + '?layout="false"';
-      createModal(link, 'Create Project Wizard');
-      return false;    
- });
-/**/
-
- $('#myModal').on('hidden', function() {
-    $('#modalId').remove();
- });
 
 
